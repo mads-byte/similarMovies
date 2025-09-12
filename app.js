a5feae92483e2b54922a61ee1e1ae12dedb533fe
mads-byte <mbutl224@icloud.com>
2025-09-12 07:06:29 -0400

    Revert "finalize"
    This reverts commit f53bde1337d839969bb11edfa6e8e9cbd4fb15b3.

--- app.js
+++ app.js
@@ -1,74 +1,36 @@
 import dotenv from 'dotenv' //this module allows me to hide my authentication token in a file that will not be visible to the client
-dotenv.config(); //loads envorionment variables into process.env and makes them available for use
+dotenv.config();
 
 import express from 'express'
-
-// creating two different apps to handle two servers
 const app = express();
-const app2 = express();
 
-//allows me 
-//"Cross Origin Resource Sharing" - changes security configurations which allows me to make requests to tmdb 
 import cors from 'cors'
+
+
 app.use(cors())
-app2.use(cors())
-
-//setting the ports where servers will run
-const PORT = process.env.PORT || 3000
-const PORT2 = process.env.PORT || 3001
-
-//storing the api authentication token in a variable for future use
 const accessToken = process.env.AUTH_TOKEN
 
-//setting the route for the request
-app.get('/movies', async (req, res) => { //targeting the movies param of the url
-    try {
-        const searchQuery = req.query.q // pulling the query segment of the url and storing it
-        const url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`; //add query to url
-        const options = {  //customizing the configuration of the http request
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${accessToken}`
-            }
-        }
-        const response = await fetch(url, options); //waiting for the promised response to be resolved and then storing it
-        const data = await response.json(); //parses JSON
-        res.json(data); //sends json data to the client/ makes it available
-    } catch (error) {
-        console.log(`Error fetching data: ${error}`)
-    }
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
 })
 
 
-//uses the same structure as the one above
-app2.get('/movie/:movieId/similar', async (req, res) => { //":movieId" is a placeholder for the id that will go in that spot
+app.get('/movies', async (req, res) => {
     try {
-        const Id = req.params.movieId
-        const url = `https://api.themoviedb.org/3/movie/${Id}/similar?language=en-US&page=1`
-
+        const searchQuery = req.query.q
+        const url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`;
         const options = {
             method: 'GET',
             headers: {
                 accept: 'application/json',
                 Authorization: `Bearer ${accessToken}`
             }
-
         }
         const response = await fetch(url, options);
         const data = await response.json();
-        res.json(data)
+        res.json(data);
     } catch (error) {
         console.log(`Error fetching data: ${error}`)
     }
-})
-
-
-//creating two server instances and listening for requests
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-app2.listen(PORT2, () => {
-    console.log(`Server running on port ${PORT2}`)
 })
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const searchBtn = document.getElementById("search-btn")
 const searchInput = document.getElementById("search-input")
 const resultsArea = document.getElementById("results-area")
-const noResult = document.getElementById("no-result")
-
 
 async function getResults(query) {
     const search = encodeURIComponent(query)
@@ -17,67 +15,22 @@ async function getResults(query) {
     }
 }
 
-async function getSimilar(id) {
-    try {
-        const similarData = await fetch(`http://localhost:3001/movie/${id}/similar`)
-        const similarMovies = await similarData.json()
-        const similarResults = similarMovies.results
-        console.log(similarResults)
-        return similarResults
-    } catch (error) {
-        console.log(`Error: ${error}`)
-    }
-
-}
-
-searchBtn.addEventListener('click', async (event) => {
-    event.preventDefault()
+searchInput.addEventListener('input', async () => {
     resultsArea.innerHTML = ""
-    noResult.innerHTML = ""
     const searchResults = await getResults(searchInput.value)
-    if (searchResults.length === 0) {
-        const message = document.createElement("div")
-        message.innerHTML = "<h2>Hmm...couldn't find that title. Try another search</h2>"
-        noResult.appendChild(message)
-    }
+    console.log(Array.isArray(searchResults))
+    console.log(searchResults)
     searchResults.forEach(movie => {
-        let overview
         const movieDiv = document.createElement("div")
-        if (movie.overview.length > 187) {
-            overview = movie.overview.substring(0, 188) + "..."
-        } else {
-            overview = movie.overview
-        }
-        movieDiv.innerHTML = `<div><img src="https://image.tmdb.org/t/p/w154/${movie.poster_path}"></div><div>${movie.title}<div><div>${movie.release_date}<div><div>${overview}</div><h2 style="display: block; font-size: 30px">Show Similar Movies</h2>`
-        movieDiv.classList.add('movieCard')
-        movieDiv.addEventListener('click', async () => {
-            resultsArea.innerHTML = ""
-            const similarMovieResults = await getSimilar(movie.id)
-            if (similarMovieResults.length === 0) {
-                const message = document.createElement("div")
-                message.innerHTML = "<h2>Looks like this title is one of a kind...we couldn't find any similar movies for ya</h2>"
-                noResult.appendChild(message)
-
-            }
-            similarMovieResults.forEach(simMovie => {
-                const simMovieCard = document.createElement("div")
-                if (simMovie.overview.length > 187) {
-                    overview = simMovie.overview.substring(0, 188) + "..."
-                } else {
-                    overview = simMovie.overview
-                }
-                simMovieCard.innerHTML = `<div><img src="https://image.tmdb.org/t/p/w154/${simMovie.poster_path}"></div><div>${simMovie.title}</div><div>${simMovie.release_date}</div><div>${overview}</div>`
-                simMovieCard.classList.add('similarCard')
-                resultsArea.appendChild(simMovieCard)
-            })
-
-
-        })
+        movieDiv.innerHTML = `<div>${movie.title}<div><div>${movie.release_date}<div>`
         resultsArea.appendChild(movieDiv)
     })
 })
 
+searchBtn.addEventListener('click', (event) => {
+    event.preventDefault()
 
+})
 
 
 
