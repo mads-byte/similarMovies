f53bde1337d839969bb11edfa6e8e9cbd4fb15b3
mads-byte <mbutl224@icloud.com>
2025-09-12 05:32:51 -0400

    finalize

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const searchBtn = document.getElementById("search-btn")
 const searchInput = document.getElementById("search-input")
 const resultsArea = document.getElementById("results-area")
+const noResult = document.getElementById("no-result")
+
 
 async function getResults(query) {
     const search = encodeURIComponent(query)
@@ -15,22 +17,67 @@ async function getResults(query) {
     }
 }
 
-searchInput.addEventListener('input', async () => {
+async function getSimilar(id) {
+    try {
+        const similarData = await fetch(`http://localhost:3001/movie/${id}/similar`)
+        const similarMovies = await similarData.json()
+        const similarResults = similarMovies.results
+        console.log(similarResults)
+        return similarResults
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }
+
+}
+
+searchBtn.addEventListener('click', async (event) => {
+    event.preventDefault()
     resultsArea.innerHTML = ""
+    noResult.innerHTML = ""
     const searchResults = await getResults(searchInput.value)
-    console.log(Array.isArray(searchResults))
-    console.log(searchResults)
+    if (searchResults.length === 0) {
+        const message = document.createElement("div")
+        message.innerHTML = "<h2>Hmm...couldn't find that title. Try another search</h2>"
+        noResult.appendChild(message)
+    }
     searchResults.forEach(movie => {
+        let overview
         const movieDiv = document.createElement("div")
-        movieDiv.innerHTML = `<div>${movie.title}<div><div>${movie.release_date}<div>`
+        if (movie.overview.length > 187) {
+            overview = movie.overview.substring(0, 188) + "..."
+        } else {
+            overview = movie.overview
+        }
+        movieDiv.innerHTML = `<div><img src="https://image.tmdb.org/t/p/w154/${movie.poster_path}"></div><div>${movie.title}<div><div>${movie.release_date}<div><div>${overview}</div><h2 style="display: block; font-size: 30px">Show Similar Movies</h2>`
+        movieDiv.classList.add('movieCard')
+        movieDiv.addEventListener('click', async () => {
+            resultsArea.innerHTML = ""
+            const similarMovieResults = await getSimilar(movie.id)
+            if (similarMovieResults.length === 0) {
+                const message = document.createElement("div")
+                message.innerHTML = "<h2>Looks like this title is one of a kind...we couldn't find any similar movies for ya</h2>"
+                noResult.appendChild(message)
+
+            }
+            similarMovieResults.forEach(simMovie => {
+                const simMovieCard = document.createElement("div")
+                if (simMovie.overview.length > 187) {
+                    overview = simMovie.overview.substring(0, 188) + "..."
+                } else {
+                    overview = simMovie.overview
+                }
+                simMovieCard.innerHTML = `<div><img src="https://image.tmdb.org/t/p/w154/${simMovie.poster_path}"></div><div>${simMovie.title}</div><div>${simMovie.release_date}</div><div>${overview}</div>`
+                simMovieCard.classList.add('similarCard')
+                resultsArea.appendChild(simMovieCard)
+            })
+
+
+        })
         resultsArea.appendChild(movieDiv)
     })
 })
 
-searchBtn.addEventListener('click', (event) => {
-    event.preventDefault()
 
-})
 
 
 
